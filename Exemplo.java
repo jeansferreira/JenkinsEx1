0ab567a0de0d2305e60ae08693aa30c65fee25b0
Jean Carlos da Silva Ferreira <ferreirajcs@saude.sc.gov.br>
2020-05-24 20:09:53 -0300

    inclusao da classe

---
+++ Exemplo.java
@@ -0,0 +1,12 @@
+
+/*
+ * O primeiro programa em Java: Hello World
+ * Autor: Jacques Sauvé
+ */
+
+// Todo programa tem um ponto de entrada: o "método" main de alguma "classe"
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Ola!!!! Jean seu primiero exemplo com Jenkins.");
+  }
+}
